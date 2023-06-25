826f16401ba23541910e235d2f574a787b072560
Igor K <kokorev@gmail.com>
2023-06-25 15:42:05 +0200

    Added some documentation comments and a small bugfix
    Documentation of Twilio sender implementation.

--- MessageSenderLibrarySol/MessageSenderLib/Twilio/TwilioSender.cs
+++ MessageSenderLibrarySol/MessageSenderLib/Twilio/TwilioSender.cs
@@ -4,6 +4,12 @@ using Twilio.Types;
 
 namespace MessageSenderLib.Twilio
 {
+    /// <summary>
+    /// Struct <c>TwilioCredentials</c> contains Account
+    /// SID and Auth Token as accepted by the <c>TwilioClient.Init</c>
+    /// method and also a from telephone number as accepted by
+    /// <c>Twilio.Types.PhoneNumber</c> constructor.
+    /// </summary>
     public struct TwilioCredentials
     {
         public string accountSid;
@@ -17,11 +23,11 @@ namespace MessageSenderLib.Twilio
     }
     public class TwilioSender : ISender<TwilioCredentials, MessageResource>
     {
-        private TwilioSender? _instance;
+        private static TwilioSender? _instance;
         private TwilioCredentials _creds;
 
         private TwilioSender() { }
-        public TwilioSender GetInstance() {
+        public static TwilioSender GetInstance() {
             _instance ??= new TwilioSender();
             return _instance;
         }
@@ -34,6 +40,15 @@ namespace MessageSenderLib.Twilio
             TwilioClient.Init(_creds.accountSid, _creds.authToken);
         }
 
+        /// <summary>
+        /// Sends a message to the specified number.
+        /// <c>ModifyCredentials</c> should be called beforehand to set the from number,
+        /// among other things.
+        /// </summary>
+        /// <param name="to">To phone number as accepted by the
+        /// <c>Twilio.Types.PhoneNumber</c> constructor.</param>
+        /// <param name="message">Message as stored in
+        /// <c>Twilio.Rest.Api.V2010.Account.CreateMessageOptions.Body</c></param>
         public MessageResource SendMessage(string to, string message)
         {
             var messageOptions = new CreateMessageOptions(
