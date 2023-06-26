a8b730ec6f98f45806487f8af31ea943e809282d
Igor K <kokorev@gmail.com>
2023-06-26 19:33:28 +0200

    Added small console program for testing
    Added new project - a console program for testing the library

---
+++ MessageSenderLibrarySol/LibTesting/Program.cs
@@ -0,0 +1,25 @@
+﻿using MessageSenderLib.Twilio;
+
+namespace LibTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            TwilioCredentials tc;
+            tc.accountSid = ""; // edit
+            tc.authToken = ""; // edit
+            tc.From = "+10000000000"; // edit
+
+            TwilioSender ts = TwilioSender.GetInstance();
+            //ISender<TwilioCredentials, MessageResource> is = TwilioSender.GetInstance();
+            ts.ModifyCredentials(tc);
+
+            //var message = ts.SendMessage("+420776000000", "this is a message for you, M.");
+            var message = ts.SendMessage("+420723000000", "yet another message for you, I."); // edit
+            Console.WriteLine(message.Body);
+
+        }
+    }
+}
